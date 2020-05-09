a931ab096d10f8cc43b96add316e035324537b72
Marcus <mpt789@gmail.com>
2020-05-09 13:18:40 -0700

    finished form validator js

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
 // Show input error message
-function showError(input, message) {}
+function showError(input, message) {
+  const formControl = input.parentelement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.classname = "form-control success";
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 // Event listeners
 form.addEventListener("submit", function (e) {
@@ -15,5 +32,20 @@ form.addEventListener("submit", function (e) {
     showError(username, "Username is Required");
   } else {
     showSuccess(username);
+  }
+  if (email.value === "") {
+    showError(email, "Email is Required");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === "") {
+    showError(password, "Password is Required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "Same password as above is Required");
+  } else {
+    showSuccess(password2);
   }
 });
